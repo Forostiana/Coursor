086f9fd36934841990dc95061b9633a7c6d4c45a
Inna Forostiana <inna777333777@icloud.com>
2020-08-25 23:29:19 +0200

    final work

---
+++ HW11/hw11.js
@@ -0,0 +1,20 @@
+
+const time = 50;
+
+function getRandomChines(length) {
+    return new Promise((resolve) => {
+      let sing = Date.now().toString().slice(-5);
+      let randomChines = [];
+      for(let i = 0; i < length; i++) {
+        sing += i;
+        randomChines.push(String.fromCharCode(sing));
+      }
+      setTimeout(() => resolve(randomChines.join("")), length * time)
+    })
+  }
+  const button =  document.querySelector(".button");
+  button.addEventListener('click', () =>{
+    getRandomChines(4).then((ch) =>{
+      document.querySelector(".randomChines").innerHTML = ch;
+    })
+  })
